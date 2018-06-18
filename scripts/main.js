df82323e52fe411fd00df5d1b800b201e8304fbc
AlexFuro <afurones43@gmail.com>
2018-06-18 18:32:53 -0400

    Add game factory

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,6 @@ const gameBoard = (() => {
 })();
 
 const player = (name, marker) => {
-  let name = name;
-  let marker = marker;
   const move = (board, cellPos) => {
     if (board[cellPos] == ""){
       board[cellPos] = marker;
@@ -52,7 +50,19 @@ const addClickEvents = (() => {
     cells[index].addEventListener("click",function(){
       let cellId = this.id[5];
       console.log(cellId);
+      
     });
   };
   return true;
 })();
+
+const game = (players) => {
+  let turns   = 0;
+  let board   = gameBoard;
+
+  return { players, turns, board };
+};
+
+let player1 = player("jeff","X");
+let player2 = player("jane","O");
+let newGame = game([player1,player2]);
